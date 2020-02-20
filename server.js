12712bb10b4748699634004ed02a3b331a509161
ns265422 <ns265422@gmail.com>
2020-02-20 17:10:58 +0800

    修正server設定

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const path = require('path')
 const app = express()
 const users = require('./routes/api/users')
 const profiles = require('./routes/api/profiles')
 const db = require('./config/keys').mongoURI
+
+// 執行前端靜態頁面
+if ( process.env.NODE_ENV === 'production' ) {
+  app.use(express.static('client/dist'))
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve( __dirname,'client','dist','index.html'))
+  })
+}
+
+
 const port = process.env.PORT || 5001
 
 app.use(bodyParser.urlencoded({ extended: false }))
